22c80067813e21e9e7508800bd72fb9cec1e0b19
m/s <m@umbr-ch.de>
2023-02-18 12:25:46 +0100

    strapi config - minor fixes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 // Path: ./config/env/production/server.js
 
 module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
   url: env("RENDER_EXTERNAL_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
