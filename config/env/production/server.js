369311b4f26f5fc00fb41e612cda67213bfd33c0
m/s <m@umbr-ch.de>
2023-02-19 20:53:14 +0100

    changed server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,5 @@
 // Path: ./config/env/production/server.js
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
   url: env("RENDER_EXTERNAL_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
