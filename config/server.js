148e9001ebe7d990b39c675300e72e66709aad19
m/s <m@umbr-ch.de>
2023-02-20 10:00:15 +0100

    changed server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "localhost"),
   port: env.int("PORT", 1337),
-  url: "http://localhost:1337",
+  // url: "http://localhost:1337",
   // webhooks: {
   //   // Add this to not receive populated relations in webhooks
   //   populateRelations: false,
